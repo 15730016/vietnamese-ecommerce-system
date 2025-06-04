acd8a8cd6f648f1bffe8627f4753b31272b913e8
Nhâm Thiện Sĩ <64187967+15730016@users.noreply.github.com>
2025-06-04 18:55:41 +0700

    Delete resources directory

--- resources/views/partials/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="bg-gray-100 text-center py-6 mt-12">
-    <div class="container mx-auto text-gray-600">
-        &copy; {{ date('Y') }} Nobinobi - Website thương mại điện tử mẹ và bé. All rights reserved.
-    </div>
-</footer>
